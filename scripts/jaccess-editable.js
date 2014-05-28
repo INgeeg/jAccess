3d8aa9a9c79171b2e0961526102796a53c577452
Ingeeg <aajk2008@gmail.com>
2014-05-28 15:26:25 -0500

    all editables included

--- scripts/jaccess-editable.js
+++ scripts/jaccess-editable.js
@@ -16,6 +16,7 @@
     };
     
     $.fn.jaccess = function (options) {
+        var self__ = this;
         if (options) {
             var height = options.height,
             width = options.width,
@@ -59,17 +60,17 @@
                 
             }
         } else console.log("Please load settings");
-
+        ///////////////////////handler/////////////////////////////////////////
         var handler = function () {
             var self_ = $(this),
                 index_td = self_.index(),
                 index_tr = self_.parent().index(),
                 tmp = index_td + index_tr * 50,
                 text = self_.text(),
-                newContent = table.find(".cellEditing").children().val();
-            
-
-
+                newContent = table.find(".cellEditing").children().val(),
+                optionValue = self_.data("map"),
+                lastdrop = table.find(".drop-ind option:selected").text(),
+                lastdropid = table.find(".drop-ind option:selected").val();
 
             if (self_.hasClass("tbl-header-sort"))//sort
             {
@@ -84,6 +85,10 @@
                 table.find(".cellEditing")
                         .text(newContent)
                         .removeClass("cellEditing");
+                table.find(".drop-ind")
+                            .parent()
+                            .data("map", lastdropid)
+                            .text(lastdrop);
 
                 if (table.find("tr:last").index() != index_tr)
                 if (self_.parent().hasClass("selected-tr")) {
@@ -106,7 +111,12 @@
                         .text(newContent)
                         .removeClass("cellEditing");
 
-                    if (options.columns[index_td - 1].type == "text" || options.columns[index_td - 1].type == "date") {
+                    table.find(".drop-ind")
+                            .parent()
+                            .data("map", lastdropid)
+                            .text(lastdrop);
+
+                    if (options.columns[index_td - 1].type == "text" || options.columns[index_td - 1].type == "note") {
                         self_.addClass("cellEditing")
                              .html("<input type='text' value='" + text + "' />")
                              .children()
@@ -114,9 +124,38 @@
                             .on("keydown", function (e) { textkey(e); });
                     }
 
+                    if (options.columns[index_td - 1].type == "date") {
+                        self_.addClass("cellEditing")
+                             .html("<input type='text' value='" + text + "' />")
+                             .children()
+                             .datepicker()
+                             .focus()
+                            .on("keydown", function (e) { textkey(e); });
+                        $('div.ui-datepicker').css({ fontSize: '10px' });
+                    }
+
+
+                    if (options.columns[index_td - 1].type == "drop") {
+                        var jd = [];
+                        jd.push("<select style='width:inherit;' class='drop-ind'><option value='-1'></option>");
+                        if (options.columns[index_td - 1].drop) {
+                            for (var i = 0; i < options.columns[index_td - 1].drop.RowCount; i++) {
+                                //var selected = "";
+                                //if(optionValue==options.columns[index_td - 1].drop.key[i])
+                                    //selected = "selected='selected'";
+                                jd.push(
+                                       "<option value = '" + options.columns[index_td - 1].drop.key[i] + "' >" + options.columns[index_td - 1].drop.value[i] + "</option>"
+                                       );
+                            } jd.push("</select>");
+                            self_.html(jd.join(""))
+                                .children()
+                                .val((optionValue?optionValue:-1))
+                                 .focus()
+                                 .on("keydown", function (e) { dropkey(e); });
+                        }
+                    }
                     cellpos = index_td + index_tr * 50;
                 }
-                
                 return;
             }
             else
@@ -124,13 +163,145 @@
                 table.find(".cellEditing")
                         .text(newContent)
                         .removeClass("cellEditing");
+
+                table.find(".drop-ind")
+                            .parent()
+                            .data("map", lastdropid)
+                            .text(lastdrop);
+
                 console.log("uneditable");
                 return;
             }
 
 
         }
+        ///////////////////drop handler//////////////////////////////////
+        var dropkey = function (e) {
+            var self_ = $(e.target),
+                self_td = self_.parent(),
+                self_td_i = self_td.index(),
+                self_tr = self_td.parent(),
+                optionValue = self_.val(),
+                optionText = self_.text();
+            $('.ui-datepicker').remove();
 
+            if (e.keyCode === 37) {//left
+                e.preventDefault();
+                    if (self_td_i != 1) {
+                        var cur = self_td.prev();
+                        if (options.columns[cur.index() - 1].edit) {
+                            cur.trigger("click");
+                        }
+                        else {
+                            while (!options.columns[cur.index() - 1].edit) {
+                                cur = cur.prev();
+                                if (options.columns[cur.index() - 1].edit) {
+                                    cur.trigger("click");
+                                    return;
+                                }
+                            }
+                        } return;
+                    }
+            }
+
+
+            if (e.keyCode === 39) {   //right 
+                e.preventDefault();
+                if (self_td_i != options.columns.length) {
+                    var cur = self_td.next();
+                    if (options.columns[cur.index() - 1].edit) {
+                        cur.trigger("click");
+                    }
+                    else {
+                        while (!options.columns[cur.index() - 1].edit) {
+                            cur = cur.next();
+                            if (options.columns[cur.index() - 1].edit) {
+                                cur.trigger("click");
+                                return;
+                            }
+                        }
+                    } return;
+                }
+            }
+            if (e.keyCode === 40 || e.keyCode === 13) {   //down and enter
+                e.preventDefault();
+                self_tr
+                    .next()
+                    .find('td')
+                    .eq(self_td_i)
+                    .trigger("click");
+                return;
+            }
+            if (e.keyCode === 38) {   //up 
+                e.preventDefault();
+                self_tr
+                    .prev()
+                    .find('td')
+                    .eq(self_td_i)
+                    .trigger("click");
+                return;
+            }
+
+            if (e.keyCode === 27) {  //escape
+                self_tr.find("td:first").trigger("click");
+            }
+
+            if (e.keyCode == 9) {  //tab
+                e.preventDefault();
+                if (self_td_i != options.columns.length) {
+                    var cur = self_td.next();
+                    if (options.columns[cur.index() - 1].edit)
+                        cur.trigger("click");
+                    else {
+                        while (!options.columns[cur.index() - 1].edit) {
+                            cur = cur.next();
+                            if (options.columns[cur.index() - 1].edit)
+                            { cur.trigger("click"); return; }
+                        } return;
+                    }
+
+                }
+            }
+
+        }
+        //////////////////////public/////////////////////////////////////////////
+        var load = function (data) {
+        if (options) {
+            if (data) {
+                var data = data[options.table];
+                item = [];
+                var count = 1;
+                for (var i = 0; i < data.RowCount; i++) {
+                    item.push("<tr id='" + (options.columns[0].id ? data[options.columns[0].id][i] : "") + "'><td>" + count + "</td>");
+                    for (var j = 0; j < options.columns.length; j++) {
+                        item.push("<td " + (options.columns[j].mask ? "data-map='"+data[options.columns[j].map][i] +"'" : "") + ">" +
+                            (options.columns[j].mask ? data[options.columns[j].mask][i]
+                            : data[options.columns[j].map][i]) +
+                            "</td>");
+                    }
+                    item.push("</tr>");
+                    count++;
+                }
+
+                item.push("<tr><td>*</td>");
+                for (var j = 0; j < options.columns.length; j++) {
+                    item.push("<td></td>");
+                }
+                item.push("</tr>");
+
+                table.find("tr:first").after(item.join(""));
+                table.on("click", "td", handler);
+                table.parent().parent().on("click", ".tbl-header-sort", handler);
+
+                $(".datagrid-con").scroll(function () {
+                 $(".datagrid").scrollLeft($(this).scrollLeft());
+                });
+            }
+            else { console.log("No data was loaded;"); }
+        } else { console.log("Please load settings;"); }
+        }
+       
+        ////////////////////key handler///////////////////////////////////
         var textkey = function (e) {
             var self_ = $(e.target),
                 self_td = self_.parent(),
@@ -139,6 +310,8 @@
                 self_tr = self_td.parent(),
                 len = text.length;
 
+
+            $('.ui-datepicker').remove();
             if (e.keyCode === 40 || e.keyCode === 13) {   //down and enter
                 self_tr
                     .next()
@@ -155,17 +328,17 @@
                     .trigger("click");
                 return;
             }
-            if (e.keyCode === 39 )    //right 
-                if (self_.getCursorPosition() == len) 
+            if (e.keyCode === 39)    //right 
+                if (self_.getCursorPosition() == len)
                     if (self_td_i != options.columns.length) {
                         var cur = self_td.next();
-                        if (options.columns[cur.index()-1].edit) {
+                        if (options.columns[cur.index() - 1].edit) {
                             cur.trigger("click");
                         }
                         else {
-                            while (!options.columns[cur.index()-1].edit) {
+                            while (!options.columns[cur.index() - 1].edit) {
                                 cur = cur.next();
-                                if (options.columns[cur.index()-1].edit) {
+                                if (options.columns[cur.index() - 1].edit) {
                                     cur.trigger("click");
                                     return;
                                 }
@@ -214,42 +387,6 @@
             }
 
         }
-
-        var load = function (data) {
-        if (options) {
-            if (data) {
-                var data = data[options.table];
-                item = [];
-                var count = 1;
-                for (var i = 0; i < data.RowCount; i++) {
-                    item.push("<tr id='" + (options.columns[0].id ? data[options.columns[0].id][i] : "") + "'><td>" + count + "</td>");
-                    for (var j = 0; j < options.columns.length; j++) {
-                        item.push("<td " + (options.columns[j].mask ? "data-map='"+data[options.columns[j].map][i] +"'" : "") + ">" +
-                            (options.columns[j].mask ? data[options.columns[j].mask][i]
-                            : data[options.columns[j].map][i]) +
-                            "</td>");
-                    }
-                    item.push("</tr>");
-                    count++;
-                }
-
-                item.push("<tr><td>*</td>");
-                for (var j = 0; j < options.columns.length; j++) {
-                    item.push("<td></td>");
-                }
-                item.push("</tr>");
-
-                table.find("tr:first").after(item.join(""));
-                table.on("click", "td", handler);
-                table.parent().parent().on("click", ".tbl-header-sort", handler);
-
-                $(".datagrid-con").scroll(function () {
-                 $(".datagrid").scrollLeft($(this).scrollLeft());
-                });
-            }
-            else { console.log("No data was loaded;"); }
-        } else { console.log("Please load settings;"); }
-    }
         return {
             load: load
         };
