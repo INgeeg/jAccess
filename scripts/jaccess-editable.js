d4b8e8536e0ab33299c921b78d75314f6c5c1bd9
Ingeeg <aajk2008@gmail.com>
2014-05-28 13:03:27 -0500

    editable text

--- scripts/jaccess-editable.js
+++ scripts/jaccess-editable.js
@@ -53,9 +53,9 @@
                    // table.find("td:nth-child(" + (count++) + ")").css({ "text-align": (options.columns[i].align ? options.columns[i].align : "left") });
                     item.push("<td style='width:" + options.columns[i].width + "px;'></td>");
                 } item.push("</tr>"); table.append(item.join("")).wrapAll(wrapper);
-                self.append(table_header.parent());
-                self.append(table.parent());
-                self.addClass("pre-load");
+                self.append(table_header.parent())
+                    .append(table.parent())
+                    .addClass("pre-load");
                 
             }
         } else console.log("Please load settings");
@@ -67,6 +67,10 @@
                 tmp = index_td + index_tr * 50,
                 text = self_.text(),
                 newContent = table.find(".cellEditing").children().val();
+            
+
+
+
             if (self_.hasClass("tbl-header-sort"))//sort
             {
                 console.log("header is clicked");
@@ -77,6 +81,11 @@
             if (index_tr == 0) return; //header row
             if (index_td == 0)//header column
             {
+                table.find(".cellEditing")
+                        .text(newContent)
+                        .removeClass("cellEditing");
+
+                if (table.find("tr:last").index() != index_tr)
                 if (self_.parent().hasClass("selected-tr")) {
                     table.find("tr").removeClass("selected-tr");
                 } else {
@@ -88,16 +97,21 @@
 
             if (options.columns[index_td - 1].edit) //editable td
             {
+                
+
                 table.find("tr").removeClass("selected-tr");
                 if (cellpos != tmp) {
-                    table.find(".cellEditing").text(newContent)
-                    .removeClass("cellEditing");
 
-                    if (options.columns[index_td - 1].type=="text") {
+                    table.find(".cellEditing")
+                        .text(newContent)
+                        .removeClass("cellEditing");
+
+                    if (options.columns[index_td - 1].type == "text" || options.columns[index_td - 1].type == "date") {
                         self_.addClass("cellEditing")
                              .html("<input type='text' value='" + text + "' />")
                              .children()
-                             .focus();
+                             .focus()
+                            .on("keydown", function (e) { textkey(e); });
                     }
 
                     cellpos = index_td + index_tr * 50;
@@ -107,10 +121,97 @@
             }
             else
             {
+                table.find(".cellEditing")
+                        .text(newContent)
+                        .removeClass("cellEditing");
                 console.log("uneditable");
                 return;
             }
 
+
+        }
+
+        var textkey = function (e) {
+            var self_ = $(e.target),
+                self_td = self_.parent(),
+                self_td_i = self_td.index(),
+                text = self_.val(),
+                self_tr = self_td.parent(),
+                len = text.length;
+
+            if (e.keyCode === 40 || e.keyCode === 13) {   //down and enter
+                self_tr
+                    .next()
+                    .find('td')
+                    .eq(self_td_i)
+                    .trigger("click");
+                return;
+            }
+            if (e.keyCode === 38) {   //up 
+                self_tr
+                    .prev()
+                    .find('td')
+                    .eq(self_td_i)
+                    .trigger("click");
+                return;
+            }
+            if (e.keyCode === 39 )    //right 
+                if (self_.getCursorPosition() == len) 
+                    if (self_td_i != options.columns.length) {
+                        var cur = self_td.next();
+                        if (options.columns[cur.index()-1].edit) {
+                            cur.trigger("click");
+                        }
+                        else {
+                            while (!options.columns[cur.index()-1].edit) {
+                                cur = cur.next();
+                                if (options.columns[cur.index()-1].edit) {
+                                    cur.trigger("click");
+                                    return;
+                                }
+                            }
+                        } return;
+                    }
+
+            if (e.keyCode === 37) {  //left
+                if (self_.getCursorPosition() == 0)
+                    if (self_td_i != 1) {
+                        var cur = self_td.prev();
+                        if (options.columns[cur.index() - 1].edit) {
+                            cur.trigger("click");
+                        }
+                        else {
+                            while (!options.columns[cur.index() - 1].edit) {
+                                cur = cur.prev();
+                                if (options.columns[cur.index() - 1].edit) {
+                                    cur.trigger("click");
+                                    return;
+                                }
+                            }
+                        } return;
+                    }
+            }
+
+            if (e.keyCode === 27) {  //escape
+                self_tr.find("td:first").trigger("click");
+            }
+
+            if (e.keyCode == 9) {  //tab
+                e.preventDefault();
+                if (self_td_i != options.columns.length) {
+                    var cur = self_td.next();
+                    if (options.columns[cur.index() - 1].edit)
+                        cur.trigger("click");
+                    else {
+                        while (!options.columns[cur.index() - 1].edit) {
+                            cur = cur.next();
+                            if (options.columns[cur.index() - 1].edit)
+                            { cur.trigger("click"); return; }
+                        } return;
+                    }
+
+                }
+            }
 
         }
 
@@ -123,7 +224,7 @@
                 for (var i = 0; i < data.RowCount; i++) {
                     item.push("<tr id='" + (options.columns[0].id ? data[options.columns[0].id][i] : "") + "'><td>" + count + "</td>");
                     for (var j = 0; j < options.columns.length; j++) {
-                        item.push("<td data-map='" + (options.columns[j].mask ? data[options.columns[j].map][i] : "") + "'>" +
+                        item.push("<td " + (options.columns[j].mask ? "data-map='"+data[options.columns[j].map][i] +"'" : "") + ">" +
                             (options.columns[j].mask ? data[options.columns[j].mask][i]
                             : data[options.columns[j].map][i]) +
                             "</td>");
@@ -131,12 +232,16 @@
                     item.push("</tr>");
                     count++;
                 }
+
+                item.push("<tr><td>*</td>");
+                for (var j = 0; j < options.columns.length; j++) {
+                    item.push("<td></td>");
+                }
+                item.push("</tr>");
+
                 table.find("tr:first").after(item.join(""));
                 table.on("click", "td", handler);
                 table.parent().parent().on("click", ".tbl-header-sort", handler);
-                //wrapper.on("scroll", function () {
-                    //wrapper_header.scrollLeft($(this).scrollLeft());
-                //});
 
                 $(".datagrid-con").scroll(function () {
                  $(".datagrid").scrollLeft($(this).scrollLeft());
